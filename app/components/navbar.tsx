188d6eb14cc9743d6fa596abe0467ee6043ffb7f
Riyaj Chaulagain <riyaj.chaulagain@selu.edu>
2025-01-22 14:46:45 -0600

    added the github icon with link

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 export default function Navbar() {
   return (
     <>
@@ -16,6 +16,7 @@ export default function Navbar() {
        <div className="flex items-end mr-7">
         <a href="https://www.facebook.com/riyaj.chaulagain" target="_blank" className="text-white p-4 hover:text-black transition duration-200" ><FaFacebook size ={40} /></a>
         <a href="https://www.linkedin.com/in/riyajchaulagain/" target="_blank" className="text-white p-4 hover:text-black transition duration-200" ><FaLinkedin size ={40} /></a>
+        <a href="https://github.com/riyajclgn123" target="_blank" className="text-white p-4 hover:text-black transition duration-200" ><FaGithub size ={40} /></a>
        </div>
       </div>
     </nav> 
